a1e52325c748234aa6c9fbe7aadc139292e5c8a8
bgops <bgops@hades.klin-pro.com>
2018-10-16 22:58:56 -0700

    complete parse

--- server/controller/controller_test.go
+++ server/controller/controller_test.go
@@ -1,35 +1,57 @@
 package controller
-import(
-    "testing"
-    "fmt"
-    "github.com/hunkeelin/govirt/govirtlib"
+
+import (
+	"fmt"
+	"github.com/hunkeelin/govirt/govirtlib"
+	"testing"
 )
-func TestPatchNetwork(t *testing.T){
-    fmt.Println("testing patch network")
-    n := govirtlib.Network {
-        Subnet: "10.180.250.0",
-        Netmask: "255.255.255.0",
-        Dns: []string{"10.181.35.100","10.181.35.101"},
-        Router: "10.181.250.1",
-        Iprange: []string{"10.180.250.160","10.180.250.200"},
-        Lease: "3600",
-        Maxlease: "7200",
-    }
-    err := editnetwork(n)
-    if err != nil {
-        panic(err)
-    }
+
+func TestParse(t *testing.T) {
+	m, err := parse("config")
+	if err != nil {
+		panic(err)
+	}
+	for _, i := range m {
+		fmt.Println(i.ClusterName)
+		fmt.Println(i.Godhcp)
+		fmt.Println(i.Govirt)
+		fmt.Println(i.Storage)
+	}
 }
-func TestPatchHost(t *testing.T){
-    fmt.Println("testing patch host")
-    h := govirtlib.CreateVmForm {
-        Hostname: "sf01-lab-netsrv-2.squaretrade.com",
-        VmMac: "d4:ae:52:88:7f:73",
-        VmIp: "10.180.250.123",
-        Leasetime: 16000,
-    }
-    err := edithost(h)
-    if err != nil {
-        panic(err)
-    }
+func TestPatchNetwork(t *testing.T) {
+	fmt.Println("testing patch network")
+	n := govirtlib.Network{
+		Subnet:   "10.180.250.0",
+		Netmask:  "255.255.255.0",
+		Dns:      []string{"10.181.35.100", "10.181.35.101"},
+		Router:   "10.181.250.1",
+		Iprange:  []string{"10.180.250.160", "10.180.250.200"},
+		Lease:    "3600",
+		Maxlease: "7200",
+	}
+	m, err := parse("config")
+	if err != nil {
+		panic(err)
+	}
+	err := editnetwork(m["sf_deploy"].Godhcp, n)
+	if err != nil {
+		panic(err)
+	}
+}
+func TestPatchHost(t *testing.T) {
+	fmt.Println("testing patch host")
+	h := govirtlib.CreateVmForm{
+		Hostname:  "sf01-lab-netsrv-2.squaretrade.com",
+		VmMac:     "d4:ae:52:88:7f:73",
+		VmIp:      "10.180.250.123",
+		Leasetime: 16000,
+	}
+	m, err := parse("config")
+	if err != nil {
+		panic(err)
+	}
+	err := edithost(m["sf_deploy"].Godhcp, n)
+	if err != nil {
+		panic(err)
+	}
 }
--- server/controller/network.go
+++ server/controller/network.go
@@ -1,63 +1,65 @@
 package controller
-import(
-    "github.com/hunkeelin/govirt/govirtlib"
-    "errors"
-    "fmt"
-    "io/ioutil"
-    "github.com/hunkeelin/mtls/klinreq"
-    "github.com/hunkeelin/klinutils"
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hunkeelin/govirt/govirtlib"
+	"github.com/hunkeelin/klinutils"
+	"github.com/hunkeelin/mtls/klinreq"
+	"io/ioutil"
 )
-func editnetwork(n govirtlib.Network) error {
-    p := govirtlib.PostPayload {
-        Target: "network",
-        Netinfo: n,
-    }
-    i := &klinreq.ReqInfo {
-        Dest: "sf01-lab-1.squaretrade.com",
-        Dport: klinutils.Stringtoport("godhcp"),
-        Method: "PATCH",
-        Payload: p,
-        Http: true,
-    }
-    resp, err := klinreq.SendPayload(i)
-    if err != nil {
-        return err
-    }
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
-    resp.Body.Close()
-    if resp.StatusCode != 200 {
-        fmt.Println(string(body))
-        return errors.New("Failed, check logs on the godhcp server")
-    }
-    return nil
+
+func editnetwork(godhcp string, n govirtlib.Network) error {
+	p := govirtlib.PostPayload{
+		Target:  "network",
+		Netinfo: n,
+	}
+	i := &klinreq.ReqInfo{
+		Dest:    godhcp,
+		Dport:   klinutils.Stringtoport("godhcp"),
+		Method:  "PATCH",
+		Payload: p,
+		Http:    true,
+	}
+	resp, err := klinreq.SendPayload(i)
+	if err != nil {
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		fmt.Println(string(body))
+		return errors.New("Failed, check logs on the godhcp server")
+	}
+	return nil
 }
-func edithost(n govirtlib.CreateVmForm) error {
-    p := govirtlib.PostPayload {
-        Target: "host",
-        VmForm: n,
-    }
-    i := &klinreq.ReqInfo {
-        Dest: "sf01-lab-1.squaretrade.com",
-        Dport: klinutils.Stringtoport("godhcp"),
-        Method: "POST",
-        Payload: p,
-        Http: true,
-    }
-    resp, err := klinreq.SendPayload(i)
-    if err != nil {
-        return err
-    }
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
-    resp.Body.Close()
-    if resp.StatusCode != 200 {
-        fmt.Println(string(body))
-        return errors.New("Failed, check logs on the godhcp server")
-    }
-    return nil
+func edithost(godhcp string, n govirtlib.CreateVmForm) error {
+	p := govirtlib.PostPayload{
+		Target: "host",
+		VmForm: n,
+	}
+	i := &klinreq.ReqInfo{
+		Dest:    godhcp,
+		Dport:   klinutils.Stringtoport("godhcp"),
+		Method:  "POST",
+		Payload: p,
+		Http:    true,
+	}
+	resp, err := klinreq.SendPayload(i)
+	if err != nil {
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		fmt.Println(string(body))
+		return errors.New("Failed, check logs on the godhcp server")
+	}
+	return nil
 }
